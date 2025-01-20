f2ca01aefb15c477c8843d3526fc62cb2a39e2db
jasonkim0809 <kimjason0809@gmail.com>
2025-01-20 18:33:56 -0500

    added 'solver' object that moves through a given maze of correct format

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
         // "actual" main code starts here VVV
 
-        Maze maze;
+        Maze maze = null;
 
         if (cmdline.hasOption("i")){
             try { // read maze from file
@@ -48,6 +48,7 @@ public class Main {
             } catch(Exception e) {
                 logger.error("/!\\ An error has occured /!\\");
             }
+            maze.printSol();
         }
         else{
             logger.info("Must use the -i flag");
@@ -62,7 +63,8 @@ public class Main {
 
 class Maze{
     int[][] data;
-    
+    Solver solver = null;
+
     public Maze(String filepath){
         try { // read maze from file
             BufferedReader reader = new BufferedReader(new FileReader(filepath));
@@ -92,12 +94,18 @@ class Maze{
         } catch(Exception e) {
             return;
         }
+        this.solver = new Solver();
+    }
+
+    void printSol(){
+        solver.findPath(data);
+        System.out.println(solver.toFactorized(solver.path));
     }
 
     void printMaze(){
         for (int i = 0; i < this.data.length;i++){
             for (int j = 0; j < this.data[i].length;j++){
-                System.out.print(this.data[i][j]+" ");
+                System.out.print(((this.data[i][j] == 1) ? "#" : " ")+" ");
             }
             System.out.print(System.lineSeparator());
         }
@@ -106,9 +114,55 @@ class Maze{
 
 class Solver{
     String path = "";
-    void findPath(int[][] maze){ // maze is assumed to be in the correct format
+    int[]coordinate = {0,0}; //placeholder coordinate, doesn't actually start at 0 0
+    // coordinate format: {row,col}
+    int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // up,right,down,left
+    int current_direction = 1; // start off by facing right, change later
+    // To change directions: Use wrapping around with modulo operator
+    // ex: rotating right: current_direction = (current_direction + 1) % 4
 
+    // How the direction array works: each index is either 0 or 1, and is added to the current coordinate to change its location.
+    // example: facing "north" would be a direction array of -1,0. Adding this to the current coordinate moves the solver up by one.
+    void findPath(int[][] maze){ // maze is assumed to be in the correct format
+        coordinate[0] = findStartPos(maze,0); // search first column for open area
+        while(coordinate[0] < maze.length - 1 && coordinate[1] < maze[0].length - 1){ // this is for starting on the left side of the maze.
+            int left = (current_direction - 1 + 4) % 4;
+            int right = (current_direction + 1) % 4;
+
+            if (maze[coordinate[0]+directions[right][0]][coordinate[1]+directions[right][1]] == 0){
+                // turn right and move forward
+                current_direction = (current_direction + 1) % 4;
+                this.path = this.path + "RF";
+            }
+            else if (maze[coordinate[0]+directions[current_direction][0]][coordinate[1]+directions[current_direction][1]] == 0){
+                // move forward
+                this.path = this.path + "F";
+            }
+            else if (maze[coordinate[0]+directions[left][0]][coordinate[1]+directions[left][1]] == 0){
+                // turn right and move forward
+                current_direction = (current_direction - 1 + 4) % 4;
+                this.path = this.path + "LF";
+            }
+            else{
+                // turn around and move forward
+                current_direction = (current_direction + 1) % 4;
+                current_direction = (current_direction + 1) % 4;
+                this.path = this.path + "RR";
+            }
+            coordinate[0] += directions[current_direction][0];
+            coordinate[1] += directions[current_direction][1];
+        }
     }
+
+    int findStartPos(int[][] maze,int col){
+        for (int i = 0; i < maze.length; i++){
+            if (maze[i][col] == 0){
+                return i;
+            }
+        }
+        return 0;
+    }
+
     String toFactorized(String input){ // consists of F, R, or L
         char current = input.charAt(0);
         int sum = 0;
